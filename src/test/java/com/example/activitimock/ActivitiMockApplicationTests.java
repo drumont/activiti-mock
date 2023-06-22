94c1b66f8dd7de48b03aa111227671b151ad0f59
drumont <drumont06@gmail.com>
2023-06-23 00:10:42 +0200

    first commit

---
+++ src/test/java/com/example/activitimock/ActivitiMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
